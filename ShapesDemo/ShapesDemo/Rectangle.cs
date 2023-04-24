d5d763006b52dd11a95800604247b1a35dc0236e
insolento <maksym.bezverkhnii@gmail.com>
2023-04-24 12:58:26 -0400

    proj5

---
+++ ShapesDemo/ShapesDemo/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesDemo
+{
+    internal class Rectangle : GeometricFigure
+    {
+
+        public override double ComputeArea()
+        {
+            return Width*Height;
+        }
+    }
+}
---
+++ ShapesDemo/ShapesDemo/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapesDemo
+{
+    internal class Square: Rectangle
+    {
+        internal Square(double side)
+        {
+            Height = side;
+            Width = side;
+        }
+        internal Square(double height, double width)
+        {
+           if (height != width)
+            {
+                Height = height;
+                Width = Height;
+            } else
+            {
+                Width = width;
+                Height = height;
+            } 
+        }
+    }
+}
