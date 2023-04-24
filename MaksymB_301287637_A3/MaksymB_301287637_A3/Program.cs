6e9c0e3795731717f9a5f1aa0701c77c25b97be6
insolento <maksym.bezverkhnii@gmail.com>
2023-04-24 12:50:15 -0400

    proj3

---
+++ MaksymB_301287637_A3/MaksymB_301287637_A3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaksymB_301287637_A3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("All the tiktoks:");
+            TikTokManager.Show();
+            Console.WriteLine();
+            Console.WriteLine("All the tiktoks with hashtag covid:");
+            TikTokManager.Show("covid");
+            Console.WriteLine();
+            Console.WriteLine("All the tiktoks with length longer than 19 sec:");
+            TikTokManager.Show(19);
+            Console.WriteLine();
+            Console.WriteLine("All the tiktoks with the audience for the groups:");
+            TikTokManager.Show(Audience.Group);
+            Console.WriteLine();
+            Console.WriteLine("All the TikToks after testing Initialization");
+            TikTokManager.Initialize();
+            TikTokManager.Show();
+        }
+    }
+}
